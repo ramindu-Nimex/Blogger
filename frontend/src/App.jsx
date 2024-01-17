c98697ce0fed9ae1bc67952fad4526072c7cdd3c
Ramindu Nimesh <117975788+ramindu-Nimex@users.noreply.github.com>
2024-01-17 20:37:24 +0530

    Create and add the footer component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,20 +8,24 @@ import Projects from './pages/Projects'
 import Header from './components/Header'
 import { ToastContainer } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
+import Footer from './components/Footer'
 
 function App() {
   return (
     <>
     <Router>
       <Header/>
-      <Routes>
+      <div className='min-h-screen'>
+        <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/about' element={<About/>}/>
         <Route path='/sign-in' element={<SignIn/>}/>
         <Route path='/sign-up' element={<SignUp/>}/>
         <Route path='/dashboard' element={<Dashboard/>}/>
         <Route path='/projects' element={<Projects/>}/>
-      </Routes>
+        </Routes>
+      </div>
+      <Footer/>
     </Router>
       <ToastContainer
         position="bottom-center"
