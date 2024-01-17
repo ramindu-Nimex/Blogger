87908fed9c5a092affb8b7bab2447716fdef8c7c
Ramindu Nimesh <117975788+ramindu-Nimex@users.noreply.github.com>
2024-01-17 17:43:12 +0530

    create signUp API route

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,8 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-import userRoute from './routes/user.route.js';
+import userRoutes from './routes/user.route.js';
+import authRoutes from './routes/auth.route.js';
 
 dotenv.config();
 
@@ -10,9 +11,11 @@ mongoose.connect(process.env.MONGO_URL).then(() => {
 }).catch((error) => {console.log(error);})
 
 const app = express();	
+app.use(express.json());
 
 app.listen(3000, () => {
    console.log('Server started on http://localhost:3000');
 })
 
-app.use('/api/user', userRoute)
+app.use('/api/user', userRoutes)
+app.use('/api/auth', authRoutes)
