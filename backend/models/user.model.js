e05615b7833e264be589ca1ec0e5beee813f95c4
Ramindu Nimesh <117975788+ramindu-Nimex@users.noreply.github.com>
2024-01-18 13:03:33 +0530

    update header component with user data

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -17,7 +17,7 @@ const userSchema = new mongoose.Schema({
    },
    profilePicture: {
       type: String,
-      default: 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png'
+      default: 'https://static.vecteezy.com/system/resources/previews/020/765/399/non_2x/default-profile-account-unknown-icon-black-silhouette-free-vector.jpg'
    },
 
 }, {timestamps: true});
