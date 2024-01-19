1336c3134539cc6d09b702f5a6258f1cb9828358
Ramindu Nimesh <117975788+ramindu-Nimex@users.noreply.github.com>
2024-01-19 11:15:21 +0530

    complete Delete User Account functionality

--- frontend/src/components/DashProfile.jsx
+++ frontend/src/components/DashProfile.jsx
@@ -1,16 +1,16 @@
-import { Alert, Button, TextInput } from "flowbite-react";
+import { Alert, Button, Modal, TextInput } from "flowbite-react";
 import { useEffect, useRef, useState } from "react";
 import { useSelector } from "react-redux"
 import { getDownloadURL, getStorage, ref, uploadBytesResumable } from "firebase/storage"
 import { app } from "../firebase.js";
 import { CircularProgressbar } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
-import { updateStart, updateSuccess, updateFailure } from "../redux/user/userSlice.js";
+import { updateStart, updateSuccess, updateFailure, deleteUserStart, deleteUserSuccess, deleteUserFailure } from "../redux/user/userSlice.js";
 import { useDispatch } from "react-redux";
-import { set } from "mongoose";
+import { HiOutlineExclamationCircle } from "react-icons/hi";
 
 const DashProfile = () => {
-   const { currentUser } = useSelector((state) => state.user);
+   const { currentUser, error } = useSelector((state) => state.user);
    const [imageFile, setImageFile] = useState(null)
    const [imageFileUrl, setImageFileUrl] = useState(null)
    const [imageFileUploadProgress, setImageFileUploadProgress] = useState(null)
@@ -18,6 +18,7 @@ const DashProfile = () => {
    const [imageFileUploading, setImageFileUploading] = useState(false)
    const [updateUserSuccess, setUpdateUserSuccess] = useState(null)
    const [updateUserError, setUpdateUserError] = useState(null)
+   const [showModal, setShowModal] = useState(false)
    const [formData, setFormData] = useState({})
    const filePickerRef = useRef()
    const dispatch = useDispatch()
@@ -104,6 +105,24 @@ const DashProfile = () => {
          setUpdateUserError(error.message)
       }
    }
+
+   const handleDeleteUser = async () => {
+      setShowModal(false)
+      try {
+         dispatch(deleteUserStart())
+         const res = await fetch(`/api/user/delete/${currentUser._id}`, {
+            method: 'DELETE',
+         })
+         const data = await res.json()
+         if(!res.ok) {
+            dispatch(deleteUserFailure(data.message))
+         } else {
+            dispatch(deleteUserSuccess(data))
+         }
+      } catch (error) {
+         dispatch(deleteUserFailure(error.message))
+      }
+   }
   return (
     <div className="max-w-lg mx-auto p-3 w-full">
       <h1 className="my-7 text-center font-semibold text-3xl">Profile</h1>
@@ -138,7 +157,7 @@ const DashProfile = () => {
          </Button>
       </form>
       <div className="text-red-500 flex justify-between mt-5">
-         <span className="cursor-pointer">Delete Account</span>
+         <span onClick={() => setShowModal(true)} className="cursor-pointer">Delete Account</span>
          <span className="cursor-pointer">Sign Out</span>
       </div>
       {
@@ -147,6 +166,22 @@ const DashProfile = () => {
       {
          updateUserError && <Alert color='failure' className="mt-5">{updateUserError}</Alert>
       }
+      {
+         error && <Alert color='failure' className="mt-5">{error}</Alert>
+      }
+      <Modal show={showModal} onClose={() => setShowModal(false)} popup size='md'>
+         <Modal.Header />
+         <Modal.Body>
+            <div className="text-center">
+               <HiOutlineExclamationCircle className="h-14 w-14 text-gray-500 dark:text-gray-200 mb-4 mx-auto" />
+               <h3 className="mb-5 text-lg text-gray-500 dark:text-gray-400">Are you sure you want to delete your account?</h3>
+               <div className="flex justify-center gap-4">
+                  <Button color="failure" onClick={handleDeleteUser}>Yes, I'm Sure</Button>
+                  <Button onClick={() => setShowModal(false)} color="gray">No, Cancel</Button>
+               </div>
+            </div>
+         </Modal.Body>
+      </Modal>
     </div>
   )
 }
