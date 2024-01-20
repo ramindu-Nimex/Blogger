0903ff49f8bb3689b0ff9f5747c5cfdb5efde497
Ramindu Nimesh <117975788+ramindu-Nimex@users.noreply.github.com>
2024-01-20 19:03:52 +0530

    Add comment section to the post page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,11 +14,13 @@ import OnlyAdminPrivateRoute from './components/OnlyAdminPrivateRoute'
 import CreatePost from './pages/CreatePost'
 import UpdatePost from './pages/UpdatePost'
 import PostPage from './pages/PostPage'
+import ScrollToTop from './components/ScrollToTop'
 
 function App() {
   return (
     <>
     <Router>
+      <ScrollToTop/>
       <Header/>
       <div className='min-h-screen'>
         <Routes>
